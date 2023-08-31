2d5948e20938a723bbec8a5c1901420abacd6e5b
Efim Zabarsky <zzfima@gmail.com>
2023-08-31 17:06:13 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Simple hello world</title>
+  </head>
+  <body>
+  <?php
+  $output = 121/4;
+  echo("121 divided by 4 is ");
+  echo("<h1>$output</h1>");?>
+  </body>
+</html>
