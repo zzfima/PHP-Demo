33570dbbe079d0c8002806e44320fb7c2008ce14
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-09-01 10:15:57 +0300

    rfct

--- db_functions.php
+++ db_functions.php
@@ -1,23 +1,41 @@
 <?php
-namespace mysqlite {
-    class mysqliteWrapper
+namespace db_helper {
+    class sqliteWrapper
     {
-        function get_credentials()
+        /**
+         * Connect to db
+         * @return \mysqli
+         */
+        function connect_to_db()
         {
-            //mysqlite connection
+            //sqlite connection
             $serverName = "localhost";
             $userName = "root";
             $password = "ExTuHaPQ2Kh";
+            $conn = new \mysqli($serverName, $userName, $password);
 
-            return [$serverName, $userName, $password];
+            return $conn;
         }
 
+        /**
+         * Receive current user from db
+         * @param \mysqli $conn
+         */
         function get_current_user($conn)
         {
             $sql = "SELECT CURRENT_USER()";
             $result = mysqli_query($conn, $sql);
             $user_result = $result->fetch_array()[0] ?? '';
             return $user_result;
+        }
+
+        /**
+         * Closing connection
+         * @param \mysqli $conn
+         */
+        function close_db_connection($conn)
+        {
+            $conn->close();
         }
     }
 } ?>
